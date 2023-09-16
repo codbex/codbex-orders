e31f199326ef57ea7db721260f505f3887c77db8
delchev <delchevn@gmail.com>
2023-09-16 18:50:35 +0300

    widgets fixes

---
+++ codbex-orders/gen/ui/PurchaseOrder/PurchaseOrder/PurchaseOrderItem/controller.js
@@ -0,0 +1,196 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-orders.PurchaseOrder.PurchaseOrderItem';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/js/codbex-orders/gen/api/PurchaseOrder/PurchaseOrderItem.js";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', function ($scope, $http, messageHub, entityApi) {
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 10;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("codbex-orders.PurchaseOrder.PurchaseOrder.entitySelected", function (msg) {
+			resetPagination();
+			$scope.selectedMainEntityId = msg.data.selectedMainEntityId;
+			$scope.loadPage($scope.dataPage);
+		}, true);
+
+		messageHub.onDidReceiveMessage("codbex-orders.PurchaseOrder.PurchaseOrder.clearDetails", function (msg) {
+			$scope.$apply(function () {
+				resetPagination();
+				$scope.selectedMainEntityId = null;
+				$scope.data = null;
+			});
+		}, true);
+
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber) {
+			let PurchaseOrder = $scope.selectedMainEntityId;
+			$scope.dataPage = pageNumber;
+			entityApi.count(PurchaseOrder).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("PurchaseOrderItem", `Unable to count PurchaseOrderItem: '${response.message}'`);
+					return;
+				}
+				$scope.dataCount = response.data;
+				let query = `PurchaseOrder=${PurchaseOrder}`;
+				let offset = (pageNumber - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				entityApi.filter(query, offset, limit).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("PurchaseOrderItem", `Unable to list PurchaseOrderItem: '${response.message}'`);
+						return;
+					}
+					$scope.data = response.data;
+				});
+			});
+		};
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("PurchaseOrderItem-details", {
+				action: "select",
+				entity: entity,
+				optionsPurchaseOrder: $scope.optionsPurchaseOrder,
+				optionsProduct: $scope.optionsProduct,
+				optionsUoM: $scope.optionsUoM,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("PurchaseOrderItem-details", {
+				action: "create",
+				entity: {},
+				selectedMainEntityKey: "PurchaseOrder",
+				selectedMainEntityId: $scope.selectedMainEntityId,
+				optionsPurchaseOrder: $scope.optionsPurchaseOrder,
+				optionsProduct: $scope.optionsProduct,
+				optionsUoM: $scope.optionsUoM,
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("PurchaseOrderItem-details", {
+				action: "update",
+				entity: entity,
+				selectedMainEntityKey: "PurchaseOrder",
+				selectedMainEntityId: $scope.selectedMainEntityId,
+				optionsPurchaseOrder: $scope.optionsPurchaseOrder,
+				optionsProduct: $scope.optionsProduct,
+				optionsUoM: $scope.optionsUoM,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete PurchaseOrderItem?',
+				`Are you sure you want to delete PurchaseOrderItem? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("PurchaseOrderItem", `Unable to delete PurchaseOrderItem: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsPurchaseOrder = [];
+		$scope.optionsProduct = [];
+		$scope.optionsUoM = [];
+
+		$http.get("/services/js/codbex-orders/gen/api/PurchaseOrder/PurchaseOrder.js").then(function (response) {
+			$scope.optionsPurchaseOrder = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Number
+				}
+			});
+		});
+
+		$http.get("/services/js/codbex-orders/gen/api/entities/Product.js").then(function (response) {
+			$scope.optionsProduct = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/js/codbex-orders/gen/api/entities/UoM.js").then(function (response) {
+			$scope.optionsUoM = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+		$scope.optionsPurchaseOrderValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsPurchaseOrder.length; i++) {
+				if ($scope.optionsPurchaseOrder[i].value === optionKey) {
+					return $scope.optionsPurchaseOrder[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsProductValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsProduct.length; i++) {
+				if ($scope.optionsProduct[i].value === optionKey) {
+					return $scope.optionsProduct[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsUoMValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsUoM.length; i++) {
+				if ($scope.optionsUoM[i].value === optionKey) {
+					return $scope.optionsUoM[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
