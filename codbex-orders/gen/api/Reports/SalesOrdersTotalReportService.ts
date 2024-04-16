db35a48eefdbf2ff23a6df2913e87b0bf72b4630
nedelcho-delchev-tues <nedelcho.delchev@gmail.com>
2024-04-16 15:09:42 +0300

    Added report perspective

---
+++ codbex-orders/gen/api/Reports/SalesOrdersTotalReportService.ts
@@ -0,0 +1,51 @@
+import { Controller, Get } from "sdk/http"
+import { SalesOrdersTotalReportRepository, SalesOrdersTotalReportFilter, SalesOrdersTotalReportPaginatedFilter } from "../../dao/Reports/SalesOrdersTotalReportRepository";
+import { HttpUtils } from "../utils/HttpUtils";
+
+@Controller
+class SalesOrdersTotalReportService {
+
+    private readonly repository = new SalesOrdersTotalReportRepository();
+
+    @Get("/")
+    public filter(_: any, ctx: any) {
+        try {
+            const filter: SalesOrdersTotalReportPaginatedFilter = {
+                StartDate: ctx.queryParameters.StartDate ? new Date(parseInt(ctx.queryParameters.StartDate)) : undefined,
+                EndDate: ctx.queryParameters.EndDate ? new Date(parseInt(ctx.queryParameters.EndDate)) : undefined,
+                "$limit": ctx.queryParameters["$limit"] ? parseInt(ctx.queryParameters["$limit"]) : undefined,
+                "$offset": ctx.queryParameters["$offset"] ? parseInt(ctx.queryParameters["$offset"]) : undefined
+            };
+
+            return this.repository.findAll(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Get("/count")
+    public count(_: any, ctx: any) {
+        try {
+            const filter: SalesOrdersTotalReportFilter = {
+                StartDate: ctx.queryParameters.StartDate ? new Date(parseInt(ctx.queryParameters.StartDate)) : undefined,
+                EndDate: ctx.queryParameters.EndDate ? new Date(parseInt(ctx.queryParameters.EndDate)) : undefined,
+            };
+
+            const count = this.repository.count(filter);
+            return JSON.stringify(count);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    private handleError(error: any) {
+        if (error.name === "ForbiddenError") {
+            HttpUtils.sendForbiddenRequest(error.message);
+        } else if (error.name === "ValidationError") {
+            HttpUtils.sendResponseBadRequest(error.message);
+        } else {
+            HttpUtils.sendInternalServerError(error.message);
+        }
+    }
+
+}
