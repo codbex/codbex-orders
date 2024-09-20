e0718ac662667ff47397160118cfb413686e99fe
MrGoblings <mrgoblings@gmail.com>
2024-09-20 14:37:23 +0000

    fix: navigation extensions

---
+++ codbex-orders-dashboard/navigation/services/purchase-orders.js
@@ -0,0 +1,16 @@
+const navigationData = {
+    id: 'purchase-order-navigation',
+    label: "Purchase Orders",
+    view: "purchase-orders",
+    group: "purchasing"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
---
+++ codbex-orders-dashboard/navigation/services/sales-orders.js
@@ -0,0 +1,16 @@
+const navigationData = {
+    id: 'sales-order-navigation',
+    label: "Sales Orders",
+    view: "sales-orders",
+    group: "purchasing"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
