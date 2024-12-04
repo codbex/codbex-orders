2de2cf6c04c3ab2da4cb37b437b99efa55d7c03f
TIVMOF <tomyivanov06@gmail.com>
2024-12-04 16:07:30 +0000

    fix: Navigations

--- codbex-orders/navigations/sales-orders/service.js
+++ codbex-orders/navigations/sales-orders/service.js
@@ -1,9 +1,8 @@
 const navigationData = {
     id: 'sales-order-navigation',
     label: "Sales Orders",
-    view: "sales-orders",
     group: "sales",
-    orderNumber: 100,
+    order: 100,
     link: "/services/web/codbex-orders/gen/codbex-orders/ui/SalesOrder/index.html?embedded",
 };
 
